e94b11dfcaca92aee752bab3f20ae83bc19c7e24
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-10-01 12:52:08 -0400

    mutations and queries code finalized. Most API algorithm commented out. Finalize SavedBooks.js file before deletinggit add .

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,8 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client'
+import {LOGIN} from '../utils/mutations';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -9,11 +10,19 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
+  const [login, {error}]= useMutation(LOGIN)
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
+  useEffect(()=>{
+    if(error){
+      setShowAlert(true)
+    }else{
+      setShowAlert(false)
+    }
+  }, [error])
+
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -26,15 +35,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const {data} = await login({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      
+         Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
